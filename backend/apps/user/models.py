d105c9a96eeb55799c1dbf233e0439ce903ca85d
ECarry <lianshiliang93@gmail.com>
2022-02-14 20:36:21 +0800

    update User models

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=32, verbose_name="姓名")
+    mobile = models.CharField(max_length=11, verbose_name="电话")
