d4c06bc6730349ca2813769bea1a721b56d08e72
ECarry <lianshiliang93@gmail.com>
2022-02-15 14:17:52 +0800

    update signup page validateEmail

--- frontend/src/views/login/components/SignUp.vue
+++ frontend/src/views/login/components/SignUp.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="signup-container">
     <div class="signup-form">
-
       <div class="title-container">SignUp</div>
 
       <div class="form-input">
@@ -33,14 +32,13 @@
           </el-form-item>
 
           <el-form-item label="验证码" prop="verifyCode">
-            <el-input
-              v-model="ruleForm.verifyCode"
-            ></el-input><el-button @click="handleSendVerifyCode">获取验证码</el-button>
+            <el-input v-model="ruleForm.verifyCode"></el-input
+            ><el-button @click="handleSendVerifyCode">获取验证码</el-button>
           </el-form-item>
 
           <el-form-item>
             <el-button type="primary" @click="submitForm('ruleForm')"
-            >注册</el-button
+              >注册</el-button
             >
           </el-form-item>
         </el-form>
@@ -50,76 +48,105 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'SignUp',
-  data () {
+  name: "SignUp",
+  data() {
     var validatePass = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入密码'))
+      if (value === "") {
+        callback(new Error("请输入密码"));
       } else {
-        if (this.ruleForm.checkPass !== '') {
-          this.$refs.ruleForm.validateField('checkPass')
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
         }
-        callback()
+        callback();
       }
-    }
+    };
     var validatePass2 = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请再次输入密码'))
-      } else if (value !== this.ruleForm.pass) {
-        callback(new Error('两次输入密码不一致!'))
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.password) {
+        callback(new Error("两次输入密码不一致!"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
+    // 自定义邮箱验证邮箱是否注册
+    var validateEmail = (rule, value, callback) => {
+      axios
+        .get("http://127.0.0.1:8000/user/verify/email/" + this.ruleForm.email, {
+          responseType: "json",
+        })
+        .then((response) => {
+          if (response.data.count === 1) {
+            callback(new Error("邮箱已存在！"))
+          }
+        });
+    };
     return {
       ruleForm: {
-        email: '',
-        password: '',
-        checkPass: '',
-        verifyCode: ''
+        email: "",
+        password: "",
+        checkPass: "",
+        verifyCode: "",
       },
       rules: {
         email: [
-          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { required: true, message: "请输入邮箱地址", trigger: "blur" },
           {
-            type: 'email',
-            message: '请输入正确的邮箱地址',
-            trigger: ['change']
-          }
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["change", "blur"],
+          },
+          { validator: validateEmail, trigger: "blur" },
         ],
-        password: [
-          { validator: validatePass, trigger: 'blur' }
-        ],
-        checkPass: [
-          { validator: validatePass2, trigger: 'blur' }
-        ],
+        password: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
         verifyCode: [
-          { required: true, message: '请输入验证码', trigger: 'blue' }
-        ]
-      }
-    }
+          { required: true, message: "请输入验证码", trigger: "blue" },
+        ],
+      },
+    };
   },
   methods: {
-    submitForm (formName) {
+    submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          this.$router.push('/login')
+          axios
+            .post("http://127.0.0.1:8000/user/create/", {
+              email: this.ruleForm.email,
+              password: this.ruleForm.password,
+              checkPass: this.ruleForm.checkPass,
+              verifyCode: this.ruleForm.verifyCode,
+            })
+            .then((response) => {
+              this.$router.push("/login");
+            })
+            .catch((error) => {
+              console.log(error.response.data);
+            });
         } else {
-          console.log('error submit!!')
-          return false
+          console.log("error submit!!");
+          return false;
         }
-      })
+      });
     },
-    handleSendVerifyCode () {
-      axios.get('http://127.0.0.1:8000/email/verify/').then((response) => {
-        console.log(response.data)
-      })
-    }
-  }
-}
+    handleSendVerifyCode() {
+      if (!this.ruleForm.email) {
+        console.log("请填写邮箱");
+        return false;
+      }
+      axios
+        .get(`http://127.0.0.1:8000/email/verify/${this.ruleForm.email}`, {
+          responseType: "json",
+        })
+        .then((response) => {
+          console.log(response.data);
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
