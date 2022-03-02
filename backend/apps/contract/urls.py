61273585d780c501861ddd6de03c6872ef82eb52
ECarry <lianshiliang93@gmail.com>
2022-03-02 17:16:28 +0800

    update contract app

--- backend/apps/contract/urls.py
+++ backend/apps/contract/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ContractListView.as_view()),
-    path('create/', views.CreateContractView.as_view())
+    path('', views.ContractList.as_view()),
+    path('create/', views.ContractCreate.as_view()),
+    path('<int:pk>', views.ContractDetail.as_view()),
+    path('<string>', views.ContractSearch.as_view())
 ]
--- backend/apps/contract/views.py
+++ backend/apps/contract/views.py
@@ -1,12 +1,14 @@
+from django.http import Http404
 from rest_framework.views import APIView, Response
 from rest_framework.generics import CreateAPIView
 from rest_framework import status
 from .serializers import ContractSerializers
 from .models import Contract
+from django.db.models import Q
 
 
 # 合同列表
-class ContractListView(APIView):
+class ContractList(APIView):
     def get(self, request):
         # 所有合同
         contracts = Contract.objects.all()
@@ -19,6 +21,38 @@ class ContractListView(APIView):
 
 
 # 创建合同
-class CreateContractView(CreateAPIView):
+class ContractCreate(CreateAPIView):
     serializer_class = ContractSerializers
 
+
+# 合同详情
+class ContractDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Contract.objects.get(pk=pk)
+        except Contract.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        contract = self.get_object(pk)
+        serializer = ContractSerializers(contract)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        contract = self.get_object(pk)
+        contract.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# 合同搜索
+class ContractSearch(APIView):
+    def get(self, request, string):
+        contracts = Contract.objects.filter(Q(area__contains=string) | Q(project_num__contains=string)
+                                            | Q(custom__contains=string))
+
+        if contracts.count() == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializers = ContractSerializers(contracts, many=True)
+
+        return Response(serializers.data)
